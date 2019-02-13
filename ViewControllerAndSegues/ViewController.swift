53642c0ff49e9a5d5d926e4e3552dd2d7f75200f
EDUARDO MENDOZA <rl238513@rlas-macair-25.local>
2019-02-13 11:14:21 -0600

    Initial Commit

---
+++ ViewControllerAndSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerAndSegues
+//
+//  Created by EDUARDO MENDOZA on 2/13/19.
+//  Copyright © 2019 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
